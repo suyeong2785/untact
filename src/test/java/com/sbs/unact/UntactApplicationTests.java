ff3974277b6051e9cb5519d5cf46d510b939fbf8
suyeong2785 <qkdtpwls_@naver.com>
2021-01-24 09:23:35 +0900

    첫 커밋

---
+++ src/test/java/com/sbs/unact/UntactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.unact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
